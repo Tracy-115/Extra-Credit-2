5a40418fd34595d3983f1790b902e49ff3c2487d
Tracy-115 <kmin984@insite.4cd.edu>
2025-12-09 21:47:12 -0800

    Update ExtraCredit.cpp

--- ExtraCredit.cpp
+++ ExtraCredit.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
 
+int main(){
+  map<string, vector<int>> movies;
+  ifstream file("210-xc2-FA25.txt");
+        string name;
+        int rating;
+
+        while (file >> name >> rating) {
+            movies[name].push_back(rating);
+        }
+  
+}
