15b4129717ccb95512a81f3c226fc4b504aaa798
Tracy-115 <kmin984@insite.4cd.edu>
2025-12-09 22:11:17 -0800

    Update ExtraCredit.cpp

--- ExtraCredit.cpp
+++ ExtraCredit.cpp
@@ -7,7 +7,11 @@ using namespace std;
 
 int main(){
   map<string, vector<int>> movies;
-  ifstream file("210-xc2-FA25.txt");
+  ifstream inFile("210-xc2-FA25.txt"); //lab 15 Movie Class
+  if (!inFile) {
+        cerr << "Error: could not open input.txt" << endl;
+        return 1;
+    }
   string name;
   int rating;
 
@@ -36,4 +40,16 @@ int main(){
       for (int r : psecond) sum += r;
       double avg = sum / psecond.size();
       if (avg > highest) highest = avg;
+
+  cout << "Highest average rating: " << highest << "\n";
+  cout << "Movies with this rating:\n";
+  for (auto &p : movies) {
+      double sum = 0;
+      for (int r : p.second) sum += r;
+      double avg = sum / p.second.size();
+      if (avg == highest) {
+          cout << p.first << "\n";
+      }
+  }
+return 0;
 }
