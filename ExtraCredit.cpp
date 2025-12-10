a02537f9f95992aa7aa4ce84e31b68df72dccac0
Tracy-115 <kmin984@insite.4cd.edu>
2025-12-09 21:53:31 -0800

    Milestone 1

--- ExtraCredit.cpp
+++ ExtraCredit.cpp
@@ -8,11 +8,16 @@ using namespace std;
 int main(){
   map<string, vector<int>> movies;
   ifstream file("210-xc2-FA25.txt");
-        string name;
-        int rating;
+  string name;
+  int rating;
 
-        while (file >> name >> rating) {
-            movies[name].push_back(rating);
-        }
+  while (file >> name >> rating) {
+      movies[name].push_back(rating);
+  }
+  for (auto &p : movies) {
+      cout << pfirst << ": ";
+      for (int r : psecond) cout << r << " \n";
+  }
+
   
 }
